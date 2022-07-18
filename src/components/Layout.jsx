100e40e89fc61beb6fd8868e5e01989eee2c3e39
aventris <ipo778@hotmail.com>
2022-07-18 11:41:49 -0500

    [UPDATE] Home page colors, backgound animation

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,15 +1,62 @@
-import React from 'react';
-import { Header } from '@components/Header';
+import React, { useRef, useEffect } from "react";
+import { gsap } from "gsap";
 
-import '@styles/Layout.scss';
+import { Header } from "@components/Header";
+import "@styles/Layout.scss";
 
 const Layout = ({ children }) => {
+  const animationBox = useRef(null);
+
+  const random = (min, max) => {
+    return gsap.utils.random(min, max);
+  };
+
+  useEffect(() => {
+    const createUnit = () => {
+      let unit = document.createElement("div"),
+        w = animationBox.current.offsetWidth - 10,
+        h = animationBox.current.offsetHeight - 10;
+      unit.classList.add("unit");
+      animationBox.current.appendChild(unit);
+
+      gsap.set(unit, {
+        x: random(0, w),
+        y: random(0, h),
+        scale: random(0.1, 1),
+        opacity: 0,
+      });
+
+      let durationTime = random(1, 4);
+      gsap.to(unit, {
+        opacity: 0.8,
+        duration: durationTime,
+        onComplete: () => {
+          gsap.to(unit, {
+            opacity: 0,
+            duration: durationTime,
+            onComplete: () => {
+              unit.parentNode.removeChild(unit);
+              createUnit();
+            },
+          });
+        },
+      });
+    };
+
+    for (let i = 0; i < 50; i++) {
+      createUnit();
+    }
+  }, []);
+
   return (
-    <div className='layout'>
+    <div className="layout">
       <Header />
-      {children}
+      <div className="main">
+        <div ref={animationBox} className="animation-bg "></div>
+        <div className="content">{children}</div>
+      </div>
     </div>
   );
-}
+};
 
 export { Layout };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,27 @@
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 
-import { CharacterItem } from '@components/CharacterItem';
-import { Loading } from '@components/Loading';
-import { NotFound } from '@pages/NotFound';
-import { Filter } from '@components/Filter';
+import { CharacterItem } from "@components/CharacterItem";
+import { Loading } from "@components/Loading";
+import { NotFound } from "@pages/NotFound";
+import { Filter } from "@components/Filter";
 
-import '@styles/Home.scss';
-import { Searcher } from '@components/Searcher';
-import { CharacterList } from '@containers/CharacterList';
+import "@styles/Home.scss";
+import { Searcher } from "@components/Searcher";
+import { CharacterList } from "@containers/CharacterList";
+
+/* TODO */
+
+/* use loading state to avoid first call */
 
 const Home = () => {
   const [state, setState] = React.useState({
     loading: true,
     error: false,
     characters: {},
-    input: '',
-    status: '',
-    gender: ''
+    input: "",
+    status: "",
+    gender: "",
   });
 
   const navigate = useNavigate();
@@ -25,21 +29,22 @@ const Home = () => {
 
   React.useEffect(async () => {
     try {
-      setState(state => ({
+      setState((state) => ({
         ...state,
         loading: true,
         error: false,
-        input: '',
-        status: '',
-        gender: ''
+        input: "",
+        status: "",
+        gender: "",
       }));
 
-      let res = await fetch(`https://rickandmortyapi.com/api/character${query.search}`);
-      if (!res.ok)
-        throw new Error('Not found');
+      let res = await fetch(
+        `https://rickandmortyapi.com/api/character${query.search}`
+      );
+      if (!res.ok) throw new Error("Not found");
       let data = await res.json();
 
-      setState(state => ({
+      setState((state) => ({
         ...state,
         loading: false,
         characters: data,
@@ -48,51 +53,50 @@ const Home = () => {
       setState({
         ...state,
         error: true,
-        loading: false
+        loading: false,
       });
     }
   }, [query]);
 
   const handleSearch = () => {
-    let searchQuery = '';
+    let searchQuery = "";
     if (state.input) {
       searchQuery += `name=${state.input}`;
     }
     if (state.status) {
-      searchQuery += `${searchQuery ? '&' : ''}status=${state.status}`;
+      searchQuery += `${searchQuery ? "&" : ""}status=${state.status}`;
     }
     if (state.gender) {
-      searchQuery += `${searchQuery ? '&' : ''}gender=${state.gender}`;
+      searchQuery += `${searchQuery ? "&" : ""}gender=${state.gender}`;
     }
-    if (searchQuery)
-      searchQuery = '?' + searchQuery;
+    if (searchQuery) searchQuery = "?" + searchQuery;
     navigate(searchQuery);
-  }
+  };
 
   const handleInput = (event) => {
     setState({
       ...state,
-      input: event.target.value
+      input: event.target.value,
     });
-  }
+  };
 
   const handleStatus = (event) => {
     setState({
       ...state,
-      status: event.target.value
+      status: event.target.value,
     });
-  }
+  };
 
   const handleGender = (event) => {
     setState({
       ...state,
-      gender: event.target.value
+      gender: event.target.value,
     });
-  }
+  };
 
   const getPagination = () => {
-    let nextPage = '';
-    let prevPage = '';
+    let nextPage = "";
+    let prevPage = "";
     if (state.characters.info.next) {
       nextPage = new URL(state.characters.info.next).search;
     }
@@ -100,29 +104,38 @@ const Home = () => {
       prevPage = new URL(state.characters.info.next).search;
     }
     return { prevPage: prevPage, nextPage, nextPage };
-  }
+  };
   return (
-    <div className='home'>
-      {state.loading &&
-        <Loading />
-      }
-      {!state.loading && state.error && < NotFound />}
-      {!state.loading && !state.error &&
+    <div className="home">
+      {state.loading && <Loading />}
+      {!state.loading && state.error && <NotFound />}
+      {!state.loading && !state.error && (
         <>
-          <h1 className='home__title' >Rick and Morty</h1>
-          <Searcher input={state.input} handleInput={handleInput} handleSearch={handleSearch}>
-            <Filter gender={state.gender} status={state.status} handleGender={handleGender} handleStatus={handleStatus} />
+          <h1 className="home__title">Rick and Morty</h1>
+          <Searcher
+            input={state.input}
+            handleInput={handleInput}
+            handleSearch={handleSearch}
+          >
+            <Filter
+              gender={state.gender}
+              status={state.status}
+              handleGender={handleGender}
+              handleStatus={handleStatus}
+            />
           </Searcher>
           <CharacterList prevPage={0} nextPage={1} pagination={getPagination()}>
-            {state.characters.results.map(character =>
-              <CharacterItem key={`character-${character.id}`} character={character} />
-            )}
+            {state.characters.results.map((character) => (
+              <CharacterItem
+                key={`character-${character.id}`}
+                character={character}
+              />
+            ))}
           </CharacterList>
         </>
-      }
+      )}
     </div>
-
   );
-}
+};
 
 export { Home };
