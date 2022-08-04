91972c90a3af895b12ec14fd96eae55364c10860
aventris <ipo778@hotmail.com>
2022-08-04 10:31:14 -0500

    [FIX] webpack prod build configuration

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -1,33 +1,34 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development",
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     publicPath: "./",
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
+    extensions: [".js", ".jsx"],
     alias: {
-      '@routes': path.resolve(__dirname, 'src/routes'),
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@containers': path.resolve(__dirname, 'src/containers/'),
-      '@pages': path.resolve(__dirname, 'src/pages/'),
-      '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@icons': path.resolve(__dirname, 'src/assets/icons/')
-    }
+      "@routes": path.resolve(__dirname, "src/routes"),
+      "@components": path.resolve(__dirname, "src/components/"),
+      "@containers": path.resolve(__dirname, "src/containers/"),
+      "@pages": path.resolve(__dirname, "src/pages/"),
+      "@styles": path.resolve(__dirname, "src/styles/"),
+      "@icons": path.resolve(__dirname, "src/assets/icons/"),
+      "@fonts": path.resolve(__dirname, "src/assets/fonts/"),
+    },
   },
   devtool: "source-map",
   devServer: {
     static: {
-      directory: path.join(__dirname, 'public')
+      directory: path.join(__dirname, "public"),
     },
     compress: true,
     port: 3005,
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   module: {
     rules: [
@@ -35,27 +36,23 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: "babel-loader",
+        },
       },
       {
         test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
       {
         test: /\.(png|jpg|gif)$/i,
-        type: 'asset'
-      }
-    ]
+        type: "asset",
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html'
-    })
-  ]
-}
+      template: "./public/index.html",
+      filename: "./index.html",
+    }),
+  ],
+};
